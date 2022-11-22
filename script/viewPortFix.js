f7e25af20d81cb85f202377894436772118f72a0
Simone Guarneri <simone.guarneri.s@gmail.com>
2022-11-22 18:34:18 +0100

    minor changes and adjustments for mobile

--- script/viewPortFix.js
+++ script/viewPortFix.js
@@ -5,11 +5,3 @@ window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
-
-let vw = window.innerWidth * 0.01;
-document.documentElement.style.setProperty('--vw', `${vw}px`);
-
-window.addEventListener('resize', () => {
-    let vw = window.innerWidth * 0.01;
-    document.documentElement.style.setProperty('--vw', `${vw}px`);
-});
